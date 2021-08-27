b859392a4f9341a436bcf9a7bb285ee865ad8c5c
Daslaw <64631869+Daslaw@users.noreply.github.com>
2021-08-27 23:52:16 +0100

    Task 3

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace division3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration of Values
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int divResult= 0;
+
+            // Request for values from the respondent
+            Console.WriteLine("Enter your first Number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Second Number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+           // Division and Displays of Result
+            divResult = firstNumber / secondNumber;  
+            Console.WriteLine ("The result : "+ divResult);
+        }
+    }
+}
